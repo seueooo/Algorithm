d60fa583c71a4a2b971572ad5002ac19cf0ae9a2
Hanseo Kim <108131226+seueooo@users.noreply.github.com>
2025-04-03 17:31:18 +0900

    [Silver I] Title: 포도주 시식, Time: 280 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,14 @@
+n = int(input())
+dp=[0]*10000
+wine = [0]*10000
+for i in range(n):
+    wine[i] = int(input())
+
+dp[0] = wine[0]
+dp[1] = wine[0]+wine[1]
+dp[2] = max(wine[2]+dp[0], dp[1], wine[1]+wine[2])
+
+for i in range(3, n):
+  dp[i] = max(dp[i-2]+wine[i],dp[i-3]+wine[i-1]+wine[i],dp[i-1])
+
+print(max(dp))
